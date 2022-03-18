54861daceab6440c48c6280ae92a84396e67ac51
sanekua <sasha-kuzmyk@mail.ru>
2022-03-19 01:35:53 +0200

    Added database

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -16,7 +16,6 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    print('page', page)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=4)
     return render_template('home.html', posts=posts)
 
